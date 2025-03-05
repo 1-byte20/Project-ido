c6d9bf24d590fdc7a287bbfcf2af142bc2aa5f50
Bite Gergő Máté <153619640+1-byte20@users.noreply.github.com>
2025-03-05 13:25:18 +0100

    Add files via upload

---
+++ Project-main/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="container">
+    <nav class="text-center">
+      <RouterLink class="btn btn-outline-success m-2" to="/">Főoldal</RouterLink>
+    </nav>
+    <RouterView />
+  </div>
+</template>
